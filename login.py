f5ccd55c7d212a5eb6d1fb3ae9d5eac2baaa7990
Darby Crash0veride <darby.crash0veride@gmail.com>
2019-08-19 23:13:36 -0500

    Playing with mongoDB cluster thing

---
+++ login.py
@@ -0,0 +1,57 @@
+import hashlib
+import urllib.parse
+from pymongo import MongoClient
+from getpass import getpass
+import dbconfig
+
+
+username = urllib.parse.quote_plus(dbconfig.db['username'])
+password = urllib.parse.quote_plus(dbconfig.db['password'])
+client = MongoClient("mongodb+srv://{}:{}@cluster0-sswa8.mongodb.net/test?retryWrites=true&w=majority".format(username, password))
+
+db = client["users"]
+
+collection = db["logins"]
+
+
+def new_user():
+    login = input("Username ").encode('utf-8')
+    hashed_login = hashlib.sha3_512(login).hexdigest()
+    if collection.count_documents({"username": hashed_login}) > 0:
+        print("this username has already been taken")
+        new_user()
+    else:
+        password = getpass('Password: ')
+        encoded = password.encode('utf-8')
+        hashed_password = hashlib.sha3_512(encoded).hexdigest()
+        post = {"username": hashed_login, "password": hashed_password}
+        collection.insert_one(post)
+        print("Welcome " + login.decode())
+
+def user_lookup():
+    user_to_find = input("what is your username? ").encode('utf-8')
+    hashed_to_find = hashlib.sha3_512(user_to_find).hexdigest()
+    if collection.count_documents({"username": hashed_to_find}) > 0:
+        results = collection.find({"username": hashed_to_find})
+        for result in results:
+            print(result)
+    else:
+        print("User NOT found!")
+
+
+
+
+ # def show_credentials():
+ #     login = input("What is your username? ")
+ #     if login in users:
+ #         password = users.get(login)
+ #         print(password)
+
+#new_user()
+
+#show_credentials()
+
+new_user()
+
+#user_lookup()
+
